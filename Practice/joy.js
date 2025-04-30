c334d754c6157b8ad92997e134d6a2faabcb8e1a
joydeb18 <90751425+joydeb18@users.noreply.github.com>
2025-04-30 04:19:33 +0000

    Update

---
+++ Practice/joy.js
@@ -0,0 +1,11 @@
+document.writeln("Hello")
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  const result= add(2, 3); // Output: 5
+
+document.getElementById('sum').innerText=`The sum is: ${result}`;
+
+document.writeln("tHIS IS JYO")
